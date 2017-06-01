131562611c5fb45fd656a935536280f6cd1cf451
Sen Zhou <zmatrix1211@gmail.com>
2017-06-01 09:19:23 +0800

    fix get_market_value

--- common/get_market_value.py
+++ common/get_market_value.py
@@ -3,15 +3,17 @@
 # output: str_list 会返回 pandas data frame
 #         str 返回 series
 
-def get_market_value(stock_list, start_date = '2017-05-25', end_date = '2017-05-26'):
+def get_market_value(stock_list, start_date = '2017-05-26', end_date = '2017-05-26'):
     p1 = get_price(stock_list, start_date = start_date, end_date = end_date, fields = 'close')
     s1 = get_shares(stock_list, start_date, end_date, fields = 'total')
     if isinstance(stock_list, str):
         return(p1*s1)
     else:
         market_value = []
-        for i in range(len(stock_list)):
-            market_value.append(p1.iloc[:,i] * s1.iloc[:,i])
+        p2 = p1.reindex_axis(sorted(p1.columns), axis=1)
+        #for i in range(len(stock_list)):
+        for i in range(p2.shape[1]):
+            market_value.append(p2.iloc[:,i] * s1.iloc[:,i])
 
         res = np.transpose(pd.DataFrame(market_value))
         return(res)
