083df8adb41db35d1e4413416e3a018dbc37d771
zmatrix24 <zmatrix1211@gmail.com>
2017-06-02 17:53:03 +0800

    sc_opt.minimize in min_var_risk_par add maxiter

--- ptfopt_tools.py
+++ ptfopt_tools.py
@@ -151,7 +151,9 @@ class TestPortfolio:
         while rho > tol:
             min_variance_risk_parity_res = sc_opt.minimize(min_variance_risk_parity_obj_fun, x0, method='SLSQP',
                                                            bounds=min_vairance_risk_parity_bnds,
-                                                           constraints=min_variance_risk_parity_cons)
+                                                           constraints=min_variance_risk_parity_cons,
+                                                           options={'maxiter': 10000}
+                                                           )
             if not min_variance_risk_parity_res.success:
                 temp = ' @ %s' % self.clean_period_prices.index[0]
                 error_message = min_variance_risk_parity_res.message + temp
@@ -162,7 +164,9 @@ class TestPortfolio:
         rho = 0
         min_variance_risk_parity_res = sc_opt.minimize(min_variance_risk_parity_obj_fun, x0, method='SLSQP',
                                                        bounds=min_vairance_risk_parity_bnds,
-                                                       constraints=min_variance_risk_parity_cons)
+                                                       constraints=min_variance_risk_parity_cons,
+                                                       options={'maxiter': 10000}
+                                                       )
         if not min_variance_risk_parity_res.success:
             temp = ' @ %s' % self.clean_period_prices.index[0]
             error_message = min_variance_risk_parity_res.message + temp
