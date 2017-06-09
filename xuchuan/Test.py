1d997f768c3926b8e751ceea427f695f16907f8c
OzzyXu <Xuchuan0304@gmail.com>
2017-06-10 00:57:46 +0800

    Done

--- xuchuan/Test.py
+++ xuchuan/Test.py
@@ -9,4 +9,3 @@ equity_funds_list = stock_fund_list
 
 optimal_weight = pt.optimizer(equity_funds_list, start_date='2015-01-01', equity_type='fund', method="all")
 print(optimal_weight[0])
-# print(optimal_weight[1])
