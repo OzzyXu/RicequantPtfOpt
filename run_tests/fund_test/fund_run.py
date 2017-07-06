c0ba2af198cd0afbb61c238f52fb0cd90335e04a
zmatrix24 <zmatrix1211@gmail.com>
2017-07-06 13:22:40 +0800

    pls work

---
+++ run_tests/fund_test/fund_run.py
@@ -0,0 +1,15 @@
+import rqdatac
+from rqdatac import *
+rqdatac.init("ricequant", "8ricequant8")
+
+
+import datetime as dt
+import itertools
+
+
+import matplotlib.pyplot as plt
+
+
+
+
+fund_test_suite = fund_test_suite = get_fund_test_suite('2014-01-01')
