8c204ca65c56efc884ff1a300d4631ea0b044a95
zmatrix24 <zmatrix1211@gmail.com>
2017-07-07 12:09:43 +0800

    fund test ready

--- optimizer_tests/fund_test/fund_run.py
+++ optimizer_tests/fund_test/fund_run.py
@@ -42,22 +42,22 @@ get_efficient_plots(fund_test_suite, bigboss_b_c)
 # one method is to save a small dic as txt
 
 ## 将返回的dictionary存成txt file
-with open('././figure/fund_test_suite.txt', 'w') as file:
+with open('././figure/data0705/fund_test_suite.txt', 'w') as file:
     file.write(json.dumps(fund_test_suite, ensure_ascii=False))
 
 ## 如需重新读入使用：
-with open('././figure/fund_test_suite.txt', 'r') as f:
+with open('././figure/data0705/fund_test_suite.txt', 'r') as f:
     d1 = json.load(f)
 
 
 # To save large dic (bigboss), use pickle file
 import pickle
-pickle.dump(bigboss, open( "bigboss_0705.p", "wb" ) )
-pickle.dump(bigboss_b_nc, open( "bigboss_b_nc_0705.p", "wb" ) )
-pickle.dump(bigboss_nb_c, open( "bigboss_nb_c_0705.p", "wb" ) )
-pickle.dump(bigboss_b_c, open( "bigboss_b_c_0705.p", "wb" ) )
+pickle.dump(bigboss, open( "././figure/data0705/bigboss_0705.p", "wb" ) )
+pickle.dump(bigboss_b_nc, open( "././figure/data0705/bigboss_b_nc_0705.p", "wb" ) )
+pickle.dump(bigboss_nb_c, open( "././figure/data0705/bigboss_nb_c_0705.p", "wb" ) )
+pickle.dump(bigboss_b_c, open( "././figure/data0705/bigboss_b_c_0705.p", "wb" ) )
 
 
 # To read it back to use again
-temp = pickle.load(open( "bigboss_0705.p", "rb" ))
+temp = pickle.load(open( "././figure/data0705/bigboss_0705.p", "rb" ))
 
