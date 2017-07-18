ec567973fd06e0d8a6bfe8f17900d3007a841ecc
JJ <jj228@indiana.edu>
2017-07-18 04:26:29 -0400

    input_validation changed

--- optimizer_for_engineer/research/input_validation.py
+++ optimizer_for_engineer/research/input_validation.py
@@ -8,9 +8,11 @@ class OptimizationError(Exception):
         print(warning_message)
 
 
+
 def input_validation(order_book_ids, start_date, end_date, asset_type, method, rebalancing_frequency, window, bnds,
                      cons, \
                      cov_shrinkage, expected_return,  risk_aversion_coefficient, res_options):
+
     if (start_date < "2005-07-01"):
         # return('开始日期（start_date）不能早于2005年7月1日。')
         return('开始日期（start_date）不能早于2005年7月1日。')
@@ -21,8 +23,8 @@ def input_validation(order_book_ids, start_date, end_date, asset_type, method, r
     elif (asset_type != 'fund' and asset_type != 'stock'):
         return('资产类型（asset_type）必须为股票或基金。')
 
-    elif (method != 'risk_parity' and method != 'min_variance' and method != 'risk_parity_with_cons' and method != 'all'):
-        return('请选择合适的优化器（method）。')
+    elif (method != 'risk_parity' and method != 'min_variance' and method != 'mean_variance' and method != 'min_TE' and method != 'all'):
+        return('请选择合适的优化算法（method）。')
 
     elif (rebalancing_frequency <= 0 or type(rebalancing_frequency) != int):
         return('调仓频率（rebalancing_frequency）必须大于0，且必须为整数。')
@@ -33,13 +35,25 @@ def input_validation(order_book_ids, start_date, end_date, asset_type, method, r
     elif (type(cov_shrinkage) != bool):
         return('cov_shrinkage 为布尔类型变量，请选择 True 或者 False。')
     elif (method == 'min_TE' and benchmark == 'equal_weight'):
-        return('min_TE 方法需要传入指数型benchmark。')
+        return('min_TE 方法需要传入指数型 benchmark。')
 
-    #elif (expected_return != None and len(expected_return) != len(order_book_ids)):
-    #   return('预期收益预测（expected_return）数目和资产（order_book_ids）数目不同。')
+     elif method == 'mean_variance':
+         if (type(expected_return) != None):
+             
+             import pandas as pd
+             
+             if (type(expected_return) == pd.Series):
+                 
+                 missing_asset = [asset for asset in expected_return.index if asset not in  order_book_ids]
+             
+                 if (len(missing_asset) != 0):
+                     return('预期收益预测（expected_return）和所选合约（order_book_ids）不一致。')
+             else:
+                 return('预期收益预测（expected_return）的类型应为 pandas.Series。')
+            
 
     #elif (expected_return_cov != None and len(expected_return_cov) != len(order_book_ids)):
-    #   return('预期收益协方差矩阵（expected_return_cov）大小和资产数目（order_book_ids）不一致。')
+    #    return('预期收益协方差矩阵（expected_return_cov）大小和资产数目（order_book_ids）不一致。')
 
     elif (risk_aversion_coefficient < 0):
         return('风险厌恶系数（risk_aversion_coefficient）不能小于0。')
@@ -63,7 +77,7 @@ def input_validation(order_book_ids, start_date, end_date, asset_type, method, r
 
         asset_list = rqdatac.fund.instruments(order_book_ids)
 
-        # 收集公募基金类资产的类型标记（场内基金，分级基金等返回的类型不是“CS”，场外基金返回 None，均不进入 list 中）
+        # 收集公募基金类资产的类型标记
         asset_type_list = [asset.type for asset in asset_list if asset.type == 'PublicFund']
 
         if (len(asset_type_list) != len(order_book_ids)):
