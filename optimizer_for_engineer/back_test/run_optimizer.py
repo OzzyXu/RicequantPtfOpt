2b9ded14b7f3e982b497e23fa90830a46fad3da6
zmatrix24 <zmatrix1211@gmail.com>
2017-07-25 10:39:20 +0800

    update again

--- optimizer_for_engineer/back_test/run_optimizer.py
+++ optimizer_for_engineer/back_test/run_optimizer.py
@@ -11,10 +11,12 @@ order_book_ids = ['161826',
 
 rebalancing_date= '2014-01-01'
 asset_type = 'fund'
-method = 'risk_parity'
+#method = 'risk_parity'
 # method = 'min_variance'
-# method = 'mean_variance'
+ method = 'mean_variance'
 bnds={'full_list': (0, 0.3) }
+expected_return = None
+# expected_return= 'empirical_mean'
 
 
 from optimizer_for_engineer.back_test.portfolio_optimize import *
@@ -25,4 +27,4 @@ from optimizer_for_engineer.back_test.portfolio_optimize import *
 res = portfolio_optimize(order_book_ids, rebalancing_date, asset_type, method, window= 132,
                        bnds=bnds, cons=None, cov_shrinkage = True,
                        benchmark = 'equal_weight',
-                       expected_return= 'empirical_mean', risk_aversion_coef=1)
+                       expected_return= expected_return, risk_aversion_coef=1)
